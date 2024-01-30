71a389f1689a3c701acad8c5d4588f9b55bd4662
Jacobcon3315 <jake.conacher@gmail.com>
2024-01-30 15:54:54 -0600

    Added new text box with specialized styling

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, StatusBar, FlatList } from "react-native";
+import { SafeAreaView, StyleSheet, StatusBar, FlatList, TextInput } from "react-native";
 import { Item, DATA } from "./components/ShoppingItem";
 
 function ShoppingListPage() {
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.textItem}
+        onChangeText={"onChangeText"}
+        value={"Add New Item To List"}
+      />
       <FlatList
         data={DATA}
         renderItem={({item}) => <Item title={item.title} />}
@@ -18,6 +23,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+  },
+  textItem: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 16,
+    fontSize: 24
   }
 });
 
